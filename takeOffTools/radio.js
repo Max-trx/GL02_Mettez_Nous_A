9acfcf29e1cc9658681a19860666afb6ebedcf16
Chabannes <thomas.chabannes@utt.fr>
2023-11-13 17:20:37 +0100

    Changement du radio

--- takeOffTools/radio.js
+++ takeOffTools/radio.js
@@ -1,8 +1,7 @@
 var Radio = {
 check: function(){
-console.log("Engine Ready !")
+    console.log("Radio Ready !")
 console.log("Grey level checked".grey)
-console.log("Radio Ready !")
 console.log("Magenta level checked".magenta)
 console.log("Blue level checked".blue)
 return true
