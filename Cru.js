005d0b93b35253e5aacb10900904d67dee2dff42
Max-trx <maxime.trarieux10@gmail.com>
2023-11-30 19:18:03 +0100

    essaie d'un nouveau parser en cours de création

---
+++ Cru.js
@@ -0,0 +1,9 @@
+var POI = function(uv, type, place, jour, cara, salle){
+
+	this.uv = uv;
+    this.type = type;
+    this.place = place;
+    this.jour = jour;
+    this.caracteristique = cara;
+    this.salle = salle;
+}
